cf3fc1cf872d5ff363ffe0124327ae3b2a6cb122
been6505 <akasitlove@gmail.com>
2023-10-18 19:35:21 +0700

    2 commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+const LAB_WEIGHT: f64 = 0.4;
+const EXAM_WEIGHT: f64 = 0.6;
+
+
 fn main() {
     println!("Hello, world!");
 
-    println!("Calculate Grade");
+    println!("Calculate");
     
-    let lab_score = 0.4*58.0;
-    let exam_score = 0.6*43.0;
+    let lab_score = LAB_WEIGHT*58.0;
+    let exam_score = EXAM_WEIGHT*43.0;
     let score = lab_score+exam_score;
     let grade = match score {
+        // inclusive on both ends สร้างช่วงที่รวมทั้งสองขอบ
         80.0..=100.0 => "A",
         75.0..=79.0 => "B+",
         70.0..=74.0 => "B",
@@ -17,8 +22,13 @@ fn main() {
         _ => "F",
     };
 
-    println!("Lab score : {}",lab_score);
-    println!("Exam score : {}",exam_score);
-    println!("Total score : {}", score);
-    println!("Grade : {}",grade);
+    println!("Lab score : {:.2}",lab_score);
+    println!("Exam score : {:.2}",exam_score);
+    println!("Total score : {:.2}", score);
+    println!("Grade : {:.2}",grade);
 }
+
+// 100 *0.4=40
+// 100 *0.6=60
+//          100/1.0
+    
